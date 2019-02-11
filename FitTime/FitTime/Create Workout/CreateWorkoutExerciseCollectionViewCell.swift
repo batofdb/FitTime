b76fe1949ec8cffb3862c7a654a6b0b0ef644b59
Francis Bato <batofdb@gmail.com>
2019-02-10 16:10:58 -0800

    cleanup

---
+++ FitTime/FitTime/Create Workout/CreateWorkoutExerciseCollectionViewCell.swift
@@ -0,0 +1,112 @@
+//
+//  CreateWorkoutExerciseCollectionViewCell.swift
+//  FitTime
+//
+//  Created by Francis Bato on 1/28/19.
+//  Copyright © 2019 LateRisers. All rights reserved.
+//
+
+import UIKit
+
+class CreateWorkoutExerciseCollectionViewCell: UICollectionViewCell {
+    var muscles: [String] = [String]()
+    var muscleViews: [UILabel] = [UILabel]()
+
+    @IBOutlet weak var addButton: AddButton!
+    @IBOutlet weak var titleLabel: FTLabel!
+    @IBOutlet weak var bodyImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        commonInit()
+        contentView.isUserInteractionEnabled = false
+
+//        contentView.frame = self.bounds
+//        contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+    }
+    @IBAction func addButtonTapped(_ sender: AddButton) {
+        sender.isSelected = !sender.isSelected
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+//        commonInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+//        commonInit()
+    }
+
+    func commonInit() {
+        for _ in 1...4 {
+            let l = PaddingLabel()
+            l.backgroundColor = .white
+            l.layer.borderColor = UIColor.lightGray.cgColor
+            l.layer.borderWidth = 2.0
+            l.adjustsFontForContentSizeCategory = true
+            l.textAlignment = NSTextAlignment.center
+            l.textColor = .black
+            l.translatesAutoresizingMaskIntoConstraints = false
+            addSubview(l)
+            muscleViews.append(l)
+        }
+
+        let l1 = muscleViews[0]
+        l1.text = "Chest"
+        l1.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor).isActive = true
+        l1.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10).isActive = true
+
+
+        let l = muscleViews[1]
+        l.text = "Biceps"
+        l.bottomAnchor.constraint(equalTo: l1.bottomAnchor).isActive = true
+        l.leftAnchor.constraint(equalTo: l1.rightAnchor, constant: 8).isActive = true
+
+        let l2 = muscleViews[2]
+        l2.text = "Quads"
+        l2.topAnchor.constraint(equalTo: l1.bottomAnchor, constant: 7).isActive = true
+        l2.leftAnchor.constraint(equalTo: l1.leftAnchor).isActive = true
+
+        let l3 = muscleViews[3]
+        l3.text = "Pectoral"
+        l3.leftAnchor.constraint(equalTo: l2.rightAnchor, constant: 7).isActive = true
+        l3.bottomAnchor.constraint(equalTo: l2.bottomAnchor).isActive = true
+
+    }
+}
+
+class AddButton: UIButton {
+    override var isSelected: Bool {
+        didSet {
+            let impact = UIImpactFeedbackGenerator()
+            impact.impactOccurred()
+
+            if isSelected {
+                setImage(UIImage(named: "added"), for: .normal)
+            } else {
+                 setImage(UIImage(named: "add"), for: .normal)
+            }
+        }
+    }
+}
+
+@IBDesignable class PaddingLabel: UILabel {
+
+    @IBInspectable var topInset: CGFloat = 5.0
+    @IBInspectable var bottomInset: CGFloat = 5.0
+    @IBInspectable var leftInset: CGFloat = 8.0
+    @IBInspectable var rightInset: CGFloat = 8.0
+
+    override func drawText(in rect: CGRect) {
+        let insets = UIEdgeInsets.init(top: topInset, left: leftInset, bottom: bottomInset, right: rightInset)
+        super.drawText(in: UIEdgeInsetsInsetRect(rect, insets))
+    }
+
+    override var intrinsicContentSize: CGSize {
+        let size = super.intrinsicContentSize
+        return CGSize(width: size.width + leftInset + rightInset,
+                      height: size.height + topInset + bottomInset)
+    }
+}
---
+++ FitTime/FitTime/Utility/Fonts.swift
@@ -0,0 +1,237 @@
+//
+//  Fonts.swift
+//  FitTime
+//
+//  Created by Francis Bato on 2/2/19.
+//  Copyright © 2019 LateRisers. All rights reserved.
+//
+
+import UIKit
+
+class Fonts {
+
+
+    enum Mode: Int {
+        case dark
+        case light
+    }
+
+    enum FontFamily: String {
+        case questrialRegular = "Questrial-Regular"
+        case rubikItalic = "Rubik-Italic"
+        case rubikRegular = "Rubik-Regular"
+        case rubikBold = "Rubik-Bold"
+        case rubikMediumItalic = "Rubik-MediumItalic"
+        case rubikMedium = "Rubik-Medium"
+        case rubikBoldItalic = "Rubik-BoldItalic"
+        case rubikBlackItalic = "Rubik-BlackItalic"
+        case rubikLightItalic = "Rubik-LightItalic"
+        case rubikBlack = "Rubik-Black"
+        case rubikLight = "Rubik-Light"
+    }
+
+    static func fontMapping(for style: UIFont.TextStyle, and mode: Fonts.Mode) -> (name: String, size: CGFloat) {
+        switch mode {
+        case .dark:
+            if style == UIFont.TextStyle.title1 {
+                return (FontFamily.rubikLight.rawValue, 34.0)
+            } else if style == UIFont.TextStyle.title2  {
+                return (FontFamily.questrialRegular.rawValue, 28.0)
+            } else if style == UIFont.TextStyle.title3 {
+                return (FontFamily.rubikRegular.rawValue, 24.0)
+            } else if style == UIFont.TextStyle.largeTitle {
+                return (FontFamily.questrialRegular.rawValue, 50.0)
+            } else if style == UIFont.TextStyle.headline {
+                return (FontFamily.rubikMedium.rawValue, 30.0)
+            } else if style == UIFont.TextStyle.subheadline {
+                return (FontFamily.rubikRegular.rawValue, 13.0)
+            } else if style == UIFont.TextStyle.body {
+                return (FontFamily.rubikRegular.rawValue, 15.0)
+            } else if style == UIFont.TextStyle.callout {
+                return (FontFamily.rubikMedium.rawValue, 15.0)
+            } else if style == UIFont.TextStyle.footnote {
+                return (FontFamily.rubikLight.rawValue, 14.0)
+            }  else if style == UIFont.TextStyle.caption1 {
+                return (FontFamily.rubikRegular.rawValue, 13.0)
+            } else if style == UIFont.TextStyle.caption2 {
+                return (FontFamily.questrialRegular.rawValue, 12.0)
+            }
+        case .light:
+            if style == UIFont.TextStyle.title1 {
+                return (FontFamily.questrialRegular.rawValue, 34.0)
+            } else if style == UIFont.TextStyle.title2  {
+                return (FontFamily.questrialRegular.rawValue, 28.0)
+            } else if style == UIFont.TextStyle.title3 {
+                return (FontFamily.rubikRegular.rawValue, 24.0)
+            } else if style == UIFont.TextStyle.largeTitle {
+                return (FontFamily.questrialRegular.rawValue, 50.0)
+            } else if style == UIFont.TextStyle.headline {
+                return (FontFamily.rubikMedium.rawValue, 30.0)
+            } else if style == UIFont.TextStyle.subheadline {
+                return (FontFamily.rubikRegular.rawValue, 13.0)
+            } else if style == UIFont.TextStyle.body {
+                return (FontFamily.rubikRegular.rawValue, 15.0)
+            } else if style == UIFont.TextStyle.callout {
+                return (FontFamily.rubikMedium.rawValue, 15.0)
+            } else if style == UIFont.TextStyle.footnote {
+                return (FontFamily.rubikLight.rawValue, 14.0)
+            }  else if style == UIFont.TextStyle.caption1 {
+                return (FontFamily.rubikRegular.rawValue, 13.0)
+            } else if style == UIFont.TextStyle.caption2 {
+                return (FontFamily.questrialRegular.rawValue, 12.0)
+            }
+        }
+        return ("", 0.0)
+    }
+
+    static func getScaledFont(textStyle: UIFontTextStyle, mode: Fonts.Mode) -> UIFont {
+        let (font, size) = Fonts.fontMapping(for: textStyle, and: mode)
+        guard let customFont = UIFont(name: font, size: size) else {
+            fatalError("""
+                Failed to load the "\(font)" font.
+                Make sure the font file is included in the project and the font name is spelled correctly.
+                """
+            )
+        }
+        return UIFontMetrics.default.scaledFont(for: customFont)
+    }
+
+    static func printFonts() {
+        for family: String in UIFont.familyNames
+        {
+            print("\(family)")
+            for names: String in UIFont.fontNames(forFamilyName: family)
+            {
+                print("== \(names)")
+            }
+        }
+    }
+}
+
+@IBDesignable
+class FTLabel: UILabel {
+    enum Styles: String {
+        case largeTitle = "UICTFontTextStyleLargeTitle"
+        case title1 = "UICTFontTextStyleTitle1"
+        case title2 = "UICTFontTextStyleTitle2"
+        case title3 = "UICTFontTextStyleTitle3"
+        case headline = "UICTFontTextStyleHeadline"
+        case subheadline = "UICTFontTextStyleSubheadline"
+        case body = "UICTFontTextStyleBody"
+        case callout = "UICTFontTextStyleCallout"
+        case footnote = "UICTFontTextStyleFootnote"
+        case caption1 = "UICTFontTextStyleCaption1"
+        case caption2 = "UICTFontTextStyleCpation2"
+
+        static func styleForInt(input: Int) -> Styles {
+            switch input {
+            case 0:
+                return .largeTitle
+            case 1:
+                return .title1
+            case 2:
+                return .title2
+            case 3:
+                return .title3
+            case 4:
+                return .headline
+            case 5:
+                return .subheadline
+            case 6:
+                return .body
+            case 7:
+                return .callout
+            case 8:
+                return .footnote
+            case 9:
+                return .caption1
+            case 10:
+                return caption2
+            default:
+                return .body
+            }
+        }
+
+        static func rawValueFor(style: UIFontTextStyle) -> Int {
+            if style == UIFont.TextStyle.largeTitle {
+                return 0
+            }else if style == UIFont.TextStyle.title1 {
+                return 1
+            } else if style == UIFont.TextStyle.title2  {
+                return 2
+            } else if style == UIFont.TextStyle.title3 {
+                return 3
+            } else if style == UIFont.TextStyle.headline {
+                return 4
+            } else if style == UIFont.TextStyle.subheadline {
+                return 5
+            } else if style == UIFont.TextStyle.body {
+                return 6
+            } else if style == UIFont.TextStyle.callout {
+                return 7
+            } else if style == UIFont.TextStyle.footnote {
+                return 8
+            }  else if style == UIFont.TextStyle.caption1 {
+                return 9
+            } else if style == UIFont.TextStyle.caption2 {
+                return 10
+            }
+
+            return 0
+        }
+    }
+
+    @IBInspectable var styleAdapter: Int {
+        get {
+            if let font = font.fontDescriptor.object(forKey: UIFontDescriptor.AttributeName.textStyle) as? UIFontTextStyle {
+                return Styles.rawValueFor(style: font)
+            }
+
+            return 0
+        }
+
+        set(sa) {
+            guard let m = Fonts.Mode(rawValue: mode.rawValue) else { return }
+            let st = UIFontTextStyle(Styles.styleForInt(input: sa).rawValue)
+            font = Fonts.getScaledFont(textStyle: st, mode: m)
+        }
+    }
+
+    @IBInspectable var modeAdapter: Int {
+        get {
+            return mode.rawValue
+        }
+
+        set(ma) {
+            guard let m = Fonts.Mode(rawValue: ma) else {
+                return
+            }
+
+            mode = m
+        }
+    }
+
+    var mode: Fonts.Mode = .light
+
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setup()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+
+    func setup() {
+        adjustsFontForContentSizeCategory = true
+        let st = UIFontTextStyle(Styles.styleForInt(input: styleAdapter).rawValue)
+        font = Fonts.getScaledFont(textStyle: st, mode: mode)
+        sizeToFit()
+    }
+}
